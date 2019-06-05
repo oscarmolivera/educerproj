6c3818a287127e4720086efffc9c0edf2cd85d87
Oscar M. Olivera <oscarmolivera@gmail.com>
2019-06-05 19:45:13 -0400

    User Model and Validations ready

---
+++ app/models/user.rb
@@ -0,0 +1,10 @@
+class User < ApplicationRecord
+  before_save { email.downcase! }
+
+  VALID_EMAIL_REGEX = /\A([\w+\-].?)+@[a-z\d\-]+(\.[a-z]+)*\.[a-z]+\z/i
+  has_secure_password
+  validates :name,  presence: true, length: { maximum: 50 }   
+  validates :email, presence: true, length: { maximum: 255 }, 
+            format: { with: VALID_EMAIL_REGEX }, uniqueness: {case_sensitive: false}
+  validates :password, length: {minimum: 8}, presence: true, allow_nil: true
+end
